0c7a11543d1cfba3eb5f306cec8c9186a3a39545
pakhoii <pakhoi.study@gmail.com>
2025-12-05 21:14:59 +0700

    chores (setup): change language to typescript, setup shadcn UI, tailwind css, and define css variables and text fonts

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <h1 className="text-4xl">APEX</h1>
+        <p className="mt-2 text-lg text-gray-700">Web Application Development Project</p>
+        <Button className="mt-4">Get Started</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
