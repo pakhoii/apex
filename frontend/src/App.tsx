86190a5dc1fc05a4c414bf370ed816db8d91dc38
pakhoii <pakhoi.study@gmail.com>
2025-12-15 23:15:58 +0700

    feat (cart): complete UI for cart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,13 @@
-import { Button } from "./components/ui/button"
-import Navbar from "./components/Navbar/navbar"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Footer } from "./components/Footer/footer";
-import AuthToggle from "./components/Auth/auth";
+import { ShoppingCartComponent } from "./components/Cart/shoppingCart";
+import Navbar from "./components/Navbar/navbar";
 
 function App() {
-  const NAV_ITEMS = {
-    "ABOUT US": "/about",
-    "MODELS": "/models",
-    "CONTACT": "/contact",
-    "DASHBOARD": "/dashboard",
-    "WISHLIST": "/wishlist",
-    "CART": "/cart",
-    "BOOK TEST DRIVE": "/book-test-drive",
-  };
-
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
-      <Routes>
-        {/* Home route */}
-        <Route 
-          path="/" 
-          element={
-            <div className="mt-20">
-            <AuthToggle/>
-            </div>
-          }
-        />
-        
-        {/* Dynamic routes from NAV_ITEMS */}
-        {Object.entries(NAV_ITEMS).map(([label, href]) => (
-          <Route 
-            key={label} 
-            path={href} 
-            element={
-              <div className="flex flex-col justify-center items-center min-h-screen">
-                <h1 className="text-3xl font-bold">{label} Page</h1>
-              </div>
-            } 
-          />
-        ))}
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+      <div className="mt-40"></div>
+      <ShoppingCartComponent/>
+    </>
   )
 }
 
---
+++ frontend/src/types/cart.ts
@@ -0,0 +1,14 @@
+export interface CartItem {
+    id: string;
+    name: string;
+    quantity: number;
+    unitPrice: number;
+    imageUrl: string;
+}
+
+export interface CartItemProps {
+    item: CartItem;
+    onIncrease: (id: string) => void
+    onDecrease: (id: string) => void
+    onRemove: (id: string) => void
+}
