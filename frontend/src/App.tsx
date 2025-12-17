8a7a4053a043d57e2d96bde60af633b0b6f33429
pakhoii <pakhoi.study@gmail.com>
2025-12-17 09:25:19 +0700

    fix (car detail): change file name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar/navbar"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Footer } from "./components/Footer/footer";
 import AuthToggle from "./components/Auth/auth";
-import CarDetail from "./components/CarDetail";
 
 function App() {
   const NAV_ITEMS = {
@@ -21,28 +20,27 @@ function App() {
       <Navbar />
       <Routes>
         {/* Home route */}
-        <Route
-          path="/"
+        <Route 
+          path="/" 
           element={
             <div className="mt-20">
-              <AuthToggle />
+            <AuthToggle/>
             </div>
           }
         />
-
+        
         {/* Dynamic routes from NAV_ITEMS */}
         {Object.entries(NAV_ITEMS).map(([label, href]) => (
-          <Route
-            key={label}
-            path={href}
+          <Route 
+            key={label} 
+            path={href} 
             element={
               <div className="flex flex-col justify-center items-center min-h-screen">
                 <h1 className="text-3xl font-bold">{label} Page</h1>
               </div>
-            }
+            } 
           />
         ))}
-        <Route path="/car-detail" element={<div className="mt-20"><CarDetail /></div>} />
       </Routes>
       <Footer />
     </BrowserRouter>
