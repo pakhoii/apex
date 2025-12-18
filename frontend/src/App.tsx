43c7ce4816241d038d488892c657bf3e10d507a5
Nguyen Vu Thanh Tinh <148300219+ngvuthtinh@users.noreply.github.com>
2025-12-19 01:24:21 +0700

    feat: implement compare car page with responsive design and mock data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/navbar";
+import ComparePage from "./pages/Compare/compare";
+import { Footer } from "./components/Footer/footer";
 import AboutPage from "./pages/About/about";
+
 
 function App() {
   return (
     <>
       <Navbar />
       {/* <div className="mt-40"></div> */}
-      <AboutPage />
+      <ComparePage />
+      <AboutPage/>
+      <Footer />
     </>
   )
 }
