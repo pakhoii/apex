7c254963f6a42827d0a1069a368c946578c59d52
Nguyen Vu Thanh Tinh <ITCSIU23039@student.hcmiu.edu.vn>
2025-12-18 21:44:19 +0700

    feat: implement Auth page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
+import { Footer } from "./components/Footer/footer";
 import Navbar from "./components/Navbar/navbar";
 import AboutPage from "./pages/About/about";
+import AuthPage from "./pages/Auth/auth";
 
 function App() {
   return (
     <>
       <Navbar />
       {/* <div className="mt-40"></div> */}
-      <AboutPage />
+      <AuthPage />
     </>
   )
 }
