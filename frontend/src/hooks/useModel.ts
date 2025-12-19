9e92978d0435fdbfae72c0eea77db4eb5d11fab0
pakhoii <pakhoi.study@gmail.com>
2025-12-19 21:54:33 +0700

    feat (api): update custom hooks to connect with api for models

--- frontend/src/hooks/useModel.ts
+++ frontend/src/hooks/useModel.ts
@@ -1,11 +1,23 @@
 import { useState } from "react";
-import { getModelDetailsApi, getModelsApi } from "@/services/model.api";
+import { 
+    getModelDetailsApi, 
+    getModelsApi,
+    createModelApi,
+    updateModelApi,
+    compareModelsApi
+} from "@/services/model.api";
+
+import type { 
+    ModelFilterParams, 
+    PaginationParams, 
+    ModelCompareParams 
+} from "@/types/model";
 
 export const useModel = () => {
     const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState(false);
 
-    const fetchModels = async (filters?: any, pagination?: any) => {
+    const fetchModels = async (filters?: ModelFilterParams, pagination?: PaginationParams) => {
         try {
             setLoading(true);
             setError(null);
@@ -31,9 +43,51 @@ export const useModel = () => {
         }
     }
 
+    const updateModel = async (modelId: number, formData: FormData) => {
+        try {
+            setLoading(true);
+            setError(null);
+            return await updateModelApi(modelId, formData);
+        } catch (err: any) {
+            setError(err.message);
+            throw err;
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const createModel = async (formData: FormData) => {
+        try {
+            setLoading(true);
+            setError(null);
+            return await createModelApi(formData);
+        } catch (err: any) {
+            setError(err.message);
+            throw err;
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const compareModels = async (params: ModelCompareParams) => {
+        try {
+            setLoading(true);
+            setError(null);
+            return await compareModelsApi(params);
+        } catch (err: any) {
+            setError(err.message);
+            throw err;
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return {
         fetchModels,
         fetchModelsDetails,
+        updateModel,
+        createModel,
+        compareModels,
         loading,
         error,
     };
