0b1d81035f350d023587e6b2c99b1fec73e84dee
pakhoii <pakhoi.study@gmail.com>
2025-12-20 09:22:15 +0700

    feat (routing): add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,31 @@
-import Navbar from "./components/Navbar/navbar";
-import TestDrive from "./pages/TestDrive/testDrive";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <TestDrive modelId={1} name="Sample Model" imageUrl="/images/silver-porsche-911-turbo-s-luxury-sports-car.jpg" />
-    </>
-  )
+import ProtectedRoutes from "@/routes/protectedRoute";
+import Landing from "@/pages/Landing/landing";
+import Auth from "@/pages/Auth/auth";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* ===== PUBLIC ROUTES ===== */}
+                <Route path="/" element={<Landing />} />
+                <Route path="/auth" element={<Auth />} />
+
+                {/* ===== PROTECTED ROUTES ===== */}
+                <Route
+                    path="/dashboard"
+                    element={
+                        <ProtectedRoutes>
+                            {/* <Dashboard /> */}
+                            <></>
+                        </ProtectedRoutes>
+                    }
+                />
+
+                {/* ===== FALLBACK ===== */}
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App
